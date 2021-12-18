ab11c8101abe138302620f3557d9de12b543f409
Kaan <mkaanacar2001@hotmail.com>
2021-12-18 23:39:47 +0300

    Add turn displayer function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 class displayManager {
 
     static FetchBoard() {
@@ -5,10 +6,15 @@ class displayManager {
                 let currentNumber = square.getAttribute('data-squareNumber');
                 square.textContent = gameManager.currentGameboard[currentNumber];
             });
-
-
-
     } 
+
+    static turnInformer() {
+        //select all turn sections and delete them
+        document.querySelectorAll(".playerContainer>div").forEach(turnSection => turnSection.innerHTML = "");
+
+        const currentPlayerSection = document.querySelector(`.${player}>div`);
+        currentPlayerSection.innerHTML += "<br> It's your turn!";
+    }
 
 }
 
@@ -22,6 +28,17 @@ class gameManager {
 
     static changeGameboard(number,symbol) {
         this.#gameboard[number] = symbol;
+    }
+
+    static setCurrentPlayer(playerName) {
+        this.#gameboard[0] = playerName;
+
+    }
+
+    static get currentPlayer() {
+        let currentPlayer = this.#gameboard[0];
+        return currentPlayer;
+
     }
 
     static get currentGameboard() {
